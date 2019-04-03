d153a73af295a7b1b3c6e59e8cc35c049b36a0ab
Vivek Patil <vivekptl892@gmail.com>
2019-04-03 23:54:50 +0530

    Updated main.dart to support the newly added FirebaseAnalytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/whats_app_home.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 void main() => runApp(WhatsApp());
 
 class WhatsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseAnalytics analytics = FirebaseAnalytics();
+    FirebaseAnalyticsObserver observer =
+        FirebaseAnalyticsObserver(analytics: analytics);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "WhatsApp",
@@ -13,7 +19,8 @@ class WhatsApp extends StatelessWidget {
         primaryColor: new Color(0xff075E54),
         accentColor: new Color(0xff25D366),
       ),
-      home: WhatsAppHome(),
+      home: WhatsAppHome(analytics, observer),
+      navigatorObservers: [observer],
     );
   }
 }
