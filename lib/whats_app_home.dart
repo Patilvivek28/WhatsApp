21439df6a4747bcf21d015e4cfd1032d5a3d894a
Vivek Patil <vivekptl892@gmail.com>
2019-04-03 23:56:27 +0530

    Updated whats_app_home.dart to support the newly added FirebaseAnalytics

--- lib/whats_app_home.dart
+++ lib/whats_app_home.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/pages/camera.dart';
 import 'package:whatsapp/pages/chats.dart';
@@ -5,12 +7,21 @@ import 'package:whatsapp/pages/calls.dart';
 import 'package:whatsapp/pages/status.dart';
 
 class WhatsAppHome extends StatefulWidget {
+  WhatsAppHome(this.analytics, this.observer);
+
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
+
   @override
-  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+  _WhatsAppHomeState createState() => _WhatsAppHomeState(analytics, observer);
 }
 
 class _WhatsAppHomeState extends State<WhatsAppHome>
     with SingleTickerProviderStateMixin {
+  _WhatsAppHomeState(this.analytics, this.observer);
+
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
   TabController _tabController;
 
   @override
